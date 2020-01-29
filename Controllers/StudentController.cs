bb2d498390fc7515678609ccffb5a7e75d620d00
Dila91 <59875110+Dila91@users.noreply.github.com>
2020-01-29 10:03:27 +0100

    Initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using School.Models; 
+
+namespace School.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        private readonly ApplicationDbContext _context;
+        public ActionResult Index()
+        {
+            //using (ApplicationDbContext context = new ApplicationDbContext())
+            return View(_context.Students.ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                //using (ApplicationDbContext context = new ApplicationDbContext())
+                //{
+                    _context.Students.Add(student);
+                    _context.SaveChanges();
+
+
+                //}
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/SchoolModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace School.Models
+{
+    public class Student
+
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+    public class Teacher
+    {
+        [Key]
+        public int TeatcherId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public List<Course> Courses { get; set; }
+    }
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Assignment> Assignments { get; set; }
+    }
+    public class Assignment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+   
+}
